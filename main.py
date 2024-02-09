75e1bf2efb54a6f3135669efd16f04c94cc3c370
Andrew Cooper <andrew.cooper@kiv.dev>
2024-02-09 15:21:06 +0200

    main window

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+import tkinter as tk
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    mainWin = tk.Tk()
+    mainWin.mainloop()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
